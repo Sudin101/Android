0cc2e7305cdd2f8817dc51cde199dbb36d655ef3
Sudin101 <sudin.sudin@outlook.com>
2020-01-17 08:28:05 +0545

    stc mode

---
+++ jobseeker/app/src/main/java/com/sudin/jobseeker/StrictMode/StrictModeActivity.java
@@ -0,0 +1,10 @@
+package com.sudin.jobseeker.StrictMode;
+
+public class StrictModeActivity {
+    public static void StrictMode(){
+        android.os.StrictMode.ThreadPolicy policy =
+                new android.os.StrictMode.ThreadPolicy.Builder().permitAll().build();
+        android.os.StrictMode.setThreadPolicy(policy);
+
+    }
+}
